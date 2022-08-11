9918c240bb7c3bdfd54296ea35e74fe307ea9569
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2022-08-11 16:55:01 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,16 @@
+/*      작성자 : 이준표
+ *      백준 수학
+ *      브론즈 5
+ *      1001번 A - B
+ *      created: 2022.08.11
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+    cout << a - b;
+}
