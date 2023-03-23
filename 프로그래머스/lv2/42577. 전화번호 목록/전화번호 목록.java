755c1ba6d6609a4ba08fecaba1e88ee9487ca1d8
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-03-24 00:28:13 +0900

    [level 2] Title: 전화번호 목록, Time: 252.68 ms, Memory: 135 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        Arrays.sort(phone_book, (a,b) -> a.length() - b.length());
+        
+        
+        for (String p : phone_book) {
+            for(int i = 1 ; i < p.length() + 1 ; i++) {
+                String sub = p.substring(0, i);
+                if(map.containsKey(sub))
+                    return false;
+            }
+            map.put(p, 0);
+        }
+        
+        return true;
+    }
+}
