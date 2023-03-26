c01d76bdac188be79687027c7660cabd8c522894
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-03-26 23:41:52 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {        
+        long start = times[0], 
+            end = times[0]
+                ,mid 
+                ,answer;
+        
+        for(int i = 1; i < times.length; i++ ) {
+            start = Math.min(start, times[i]);
+            end = Math.max(end, times[i]);
+        }
+        end = end * n;
+        answer = end;
+        
+        while(start <= end) {
+            mid = (start + end) / 2L;
+            
+            long complete = 0; 
+            for(int i = 0; i < times.length; i++ ) {
+                complete += (mid / times[i]);
+            }
+            
+            
+            if(complete < n)
+                start = mid + 1;
+            else {
+                answer = Math.min(answer, mid);
+                end = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
