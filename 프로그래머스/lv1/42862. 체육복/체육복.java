8ce454e1a584474d0dd41ab974bb326fa75d667c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-04-05 21:45:47 +0900

    [level 1] Title: 체육복, Time: 0.07 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int n, int[] lost, int[] reserve) {
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
+        
+        for(int i : reserve) {
+            reserveSet.add(i);
+        }
+        
+        for(int i : lost) {
+            if(reserveSet.contains(i))
+                reserveSet.remove(i);
+            else
+                lostSet.add(i);
+        }
+        
+        for(int i : reserveSet) {
+            if(lostSet.contains(i - 1)){
+                lostSet.remove(i - 1);                
+            }
+            else if(lostSet.contains(i + 1)) {
+                lostSet.remove(i + 1);    
+            }  
+        }
+        
+        
+        return n - lostSet.size();
+        
+    }
+}
