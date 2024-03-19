41078212ccb6f8491c142dd8b1bc37ed7c8d762d
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-03-19 16:30:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.09 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        if(s.equals(" "))
+        // s == " " 일 경우 처리가 필요없음
+        if(s.isBlank())
             return " ";
+        
         
         String[] words = s.split(" ");
         StringBuilder sb = new StringBuilder();
         for(var word: words) {
+            // 문자 처음과 끝 띄어쓰기 핸들링
             if(word.length() < 1) {
                 sb.append(' ');
                 continue;
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 0.07 ms
+메모리: 76.9 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 19일 16:19:19
+2024년 03월 19일 16:30:53
 
 ### 문제 설명
 
