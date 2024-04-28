4f0fb2a31d46f9e93c04b5a6718371a6b199aa99
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-04-28 10:02:22 +0900

    [Silver V] Title: 덩치, Time: 144 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
 
@@ -13,25 +12,21 @@ public class Main {
 
 		int N = stoi(br.readLine());
 		int[][] pe = new int[N][2];
-		int[][] arr = new int[N][3];
 		for (int i = 0; i < N; i++) {
 			String[] s = br.readLine().split(" ");
-			arr[i][0] = pe[i][0] = stoi(s[0]);
-			arr[i][1] = pe[i][1] = stoi(s[1]);
-			arr[i][2] = i;
+			pe[i][0] = stoi(s[0]);
+			pe[i][1] = stoi(s[1]);
 		}
 
-		// 몸무게 순 정렬
-		Arrays.sort(arr, (a, b) -> Integer.compare(b[0], a[0]));
 		int[] ranking = new int[N];
 		for (int i = 0; i < N; i++) {
 			int rank = 1;
 
 			for (int j = 0; j < N; j++) {
-				if(i == arr[j][2])
+				if (i == j)
 					continue;
 
-				if (arr[j][0] > pe[i][0] && arr[j][1] > pe[i][1]) {
+				if (pe[j][0] > pe[i][0] && pe[j][1] > pe[i][1]) {
 					rank++;
 				}
 			}
