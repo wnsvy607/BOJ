16691f3c8f6b7a75593fa6b85425b199544d413c
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-04-29 14:18:08 +0900

    [level 2] Title: [1차] 캐시, Time: 4.96 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,41 +1,75 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int cacheSize, String[] cities) {
-        Queue<String> q = new LinkedList<>();
-        Set<String> db = new HashSet<>();
-        
-        int cost = 0;
-        
+    public int solution(int cacheSize, String[] cities) {        
         if (cacheSize == 0) {
             return cities.length * 5;
         }
+        TreeSet<Cache> pq = new TreeSet<>();
+        Map<String, Cache> db = new HashMap<>();
         
+        int time = 0;
+        int cost = 0;
+        
+
         for(String city : cities) {
             // 캐시 미스
             String key = city.toLowerCase();
-            if(!db.contains(key)) {
+            
+            if(!db.containsKey(key)) {
                 // 캐시에 더 넣을 공간이 없을 때
-                if(q.size() >= cacheSize) {
-                    String r = q.poll();    
-                    db.remove(r);                
+                if(pq.size() > cacheSize - 1) {
+                    Cache r = pq.pollFirst();    
+                    db.remove(r.key);
                 }
                 // 새로운 캐시 삽입
-                q.add(key);
-                db.add(key);
+                Cache c = new Cache(key, time);
+                db.put(key, c);
+                pq.add(c);
                 cost += 5;
             } 
             // 캐시 히트
             else {
                 // 캐시 업데이트
-                q.remove(key);
-                q.add(key);
-                cost++;
+                pq.remove(db.get(key));
+                Cache nc = new Cache(key, time);
+                db.put(key, nc);
+                pq.add(nc);
+                cost++;    
             }
+            time++;
         }
         
         
         return cost;
     }
     
+    class Cache implements Comparable<Cache> {
+        String key;
+        int time;
+        
+        Cache(String k, int t) {
+            key = k;
+            time = t;
+        }
+        
+        @Override
+        public int compareTo(Cache c) {
+            if(this.time == c.time)
+                return this.key.compareTo(c.key);
+            return Integer.compare(this.time, c.time);
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            Cache c = (Cache) o;
+            return this.key.equals(c.key) && time == c.time;
+        }
+        
+		@Override
+		public int hashCode() {
+			return Objects.hash(key);
+		}
+        
+    }
 }
