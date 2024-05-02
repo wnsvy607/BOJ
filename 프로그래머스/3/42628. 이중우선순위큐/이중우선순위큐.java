06096a19a5d82b6537edbf051bbf5dc3b293eca8
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-05-02 19:50:53 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.70 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,59 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        DoubleQueue dq = new DoubleQueue();
+        
+        TreeSet<Integer> set = new TreeSet<>();
         
         for(var op : operations) {
-            var s = op.split(" ");
+            String[] split = op.split(" ");
+            int val = Integer.parseInt(split[1]);
             
-            if(s[0].equals("I"))
-                dq.insert(Integer.parseInt(s[1]));
-            else if(s[1].equals("1"))
-                dq.removeMax();
-            else
-                dq.removeMin();
+            if(split[0].equals("I")) {
+                set.add(val);
+            } else if(val == 1) {
+                set.pollLast();
+            } else {
+                set.pollFirst();
+            }
+            
         }
         
-        return dq.getMinAndMax();
+        if(set.isEmpty())
+            return new int[]{0, 0};
+        
+        int max = set.pollLast();
+        if(set.isEmpty())
+            return new int[]{max , max};
+        int min = set.pollFirst();
+        
+        return new int[]{max , min};
     }
-    
-    class DoubleQueue {
-        PriorityQueue<Integer> maxHeap;
-        PriorityQueue<Integer> minHeap;
-        
-        DoubleQueue() {
-            maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
-            minHeap = new PriorityQueue<>();
-        }
-        
-        void insert(int value) {
-            maxHeap.offer(value);
-            minHeap.offer(value);
-        }
-        
-        void removeMin() {
-            if(isEmpty())
-                return;
-            int min = minHeap.poll();
-            maxHeap.remove(min);
-        }
-        
-        void removeMax() {
-            if(isEmpty())
-                return;
-            int max = maxHeap.poll();
-            minHeap.remove(max);
-        }
-        
-        boolean isEmpty() {
-            return maxHeap.isEmpty() && minHeap.isEmpty();
-        }
-        
-        int[] getMinAndMax() {
-            if(isEmpty())
-                return new int[]{0, 0};
-            return new int[]{maxHeap.poll(), minHeap.poll()};
-        }
-        
-    }
+
 }
