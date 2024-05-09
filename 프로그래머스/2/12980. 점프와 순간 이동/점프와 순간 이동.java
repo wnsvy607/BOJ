b746b7cad6696e6aeb4d803373d643964c31b18f
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-05-09 10:51:50 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+         
+        while(n > 0) {
+            if(n % 2 == 1) {
+                ans++;
+                n--;
+            } else {
+                n = n / 2;
+            }
+        }
+        
+        return ans;
+    }
+}
