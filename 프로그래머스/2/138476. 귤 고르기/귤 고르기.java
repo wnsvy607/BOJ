f912264b0e49ac165e59150c5e16afee026dff02
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-06-17 22:19:35 +0900

    [level 2] Title: 귤 고르기, Time: 71.04 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,48 +1,6 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int k, int[] tangerine) {        
-        Map<Integer, Tan> map = new HashMap<>();
-        
-        for(int t : tangerine) {
-            Tan tan = map.getOrDefault(t, new Tan(t));
-            tan.add();
-            map.put(t, tan);    
-        }
-        
-        PriorityQueue<Tan> pq = new PriorityQueue<>();
-        
-        for(var entry : map.entrySet()) {
-            pq.add(entry.getValue());
-        }
-        
-        int count = 0, now = 0;
-        while(now < k) {
-            now += pq.poll().value;
-            count++;
-        }
-        
-        return count;
-    }
-    
-    
-    class Tan implements Comparable<Tan> {
-        int size;
-        int value;
-        
-        Tan(int s) {
-            size = s;
-            value = 0;
-        }
-        
-        @Override
-        public int compareTo(Tan t) {
-            return Integer.compare(t.value, this.value);
-        }
-        
-        void add() {
-            this.value++;
-        }
-        
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        return answer;
     }
 }
