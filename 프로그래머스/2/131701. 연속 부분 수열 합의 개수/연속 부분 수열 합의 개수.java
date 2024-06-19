bb1f4acd851fdb079fa282d7df2629c942b56983
Lee Junpyo <85255237+wnsvy607@users.noreply.github.com>
2024-06-19 23:24:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 173.97 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,22 +1,6 @@
-import java.util.*;
-
 class Solution {
-    
     public int solution(int[] elements) {
         int answer = 0;
-        Set<Integer> set = new HashSet<>();
-        
-        int len = elements.length;
-        
-        for(int i = 0; i < len; i++) {
-            int val = 0;
-            for(int j = 0; j < len; j++) {
-                int idx = (i + j) % len;
-                val += elements[idx];
-                set.add(val);
-            }
-        }
-        
-        return set.size();
+        return answer;
     }
 }
